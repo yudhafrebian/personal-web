dc1ec5116acffd7cdb54108b971f19ba1d4e0b8e
Yudha Frebian <anandayudha777@gmail.com>
2025-02-23 12:54:25 +0700

    Fix build errors and optimize hero section

--- src/components/section/hero.tsx
+++ src/components/section/hero.tsx
@@ -6,6 +6,7 @@ import {
 } from "react-icons/ri";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function HeroSection() {
   return (
@@ -15,44 +16,44 @@ export default function HeroSection() {
     >
       <div className="w-3/5">
         <h1 className="font-montserrat text-white font-semibold text-6xl mb-1">
-          I'm <span className="text-orange-600">Yudha!</span>
+          I&apos;m <span className="text-orange-600">Yudha!</span>
         </h1>
         <h1 className="font-montserrat text-orange-600 font-semibold text-6xl mb-9">
           A Full-Stack Developer
         </h1>
         <p className="font-roboto w-3/4 text-white font-medium text-xl mb-9">
-          "Hi, I'm Yudha, a Junior Fullstack Developer with a passion for
+          &quot;Hi, I&apos;m Yudha, a Junior Fullstack Developer with a passion for
           building scalable and efficient web applications. I specialize in
           JavaScript, Next, and Node.js, creating seamless user experiences from
-          front-end to back-end. Excited to collaborate on innovative projects!"
+          front-end to back-end. Excited to collaborate on innovative projects!&quot;
         </p>
         <div className="flex items-center gap-12">
           <Link href="/#contact-me">
             <Button size="lg">Contact Me</Button>
           </Link>
           <div className="flex gap-6">
-            <a href="https://github.com/yudhafrebian" target="blank">
+            <a href="https://github.com/yudhafrebian" target="_blank" rel="noopener noreferrer">
               <RiGithubFill
                 className="hover:scale-125 duration-200 ease-out"
                 color="white"
                 size={36}
               />
             </a>
-            <a href="https://www.linkedin.com/in/ananda-yudha-382533190/" target="blank">
+            <a href="https://www.linkedin.com/in/ananda-yudha-382533190/" target="_blank" rel="noopener noreferrer">
               <RiLinkedinBoxFill
                 className="hover:scale-125 duration-200 ease-out"
                 color="white"
                 size={36}
               />
             </a>
-            <a href="https://www.instagram.com/yudha_frebian/" target="blank">
+            <a href="https://www.instagram.com/yudha_frebian/" target="_blank" rel="noopener noreferrer">
               <RiInstagramFill
                 className="hover:scale-125 duration-200 ease-out"
                 color="white"
                 size={36}
               />
             </a>
-            <a href="https://wa.me/6289637663755" target="blank">
+            <a href="https://wa.me/6289637663755" target="_blank" rel="noopener noreferrer">
               <RiWhatsappFill
                 className="hover:scale-125 duration-200 ease-out"
                 color="white"
@@ -63,9 +64,12 @@ export default function HeroSection() {
         </div>
       </div>
       <div className="w-custom2 h-custom2 rounded-full bg-white relative overflow-hidden">
-        <img
+        <Image
           className="absolute h-full w-full object-contain object-center top-2"
-          src="\assets\profile-pic.png"
+          src="/assets/profile-pic.png"
+          alt="Profile Picture"
+          width={500}
+          height={500}
         />
       </div>
     </section>
